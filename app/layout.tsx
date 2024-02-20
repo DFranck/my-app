9933e405f13cbc2c5f5ca981fb7297f9670fafe3
DFranck <franckdufournet@hotmail.fr>
2024-02-20 15:55:37 +0700

    init next.js avec tailwind et typescript

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
+import { Header } from "@/src/feature/layout/Header";
+import { Footer } from "@/src/theme/Footer";
+import { ThemeProvider } from "@/src/theme/ThemeProvider";
+import clsx from "clsx";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +18,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full">
+      <body className={clsx(inter.className, "bg-background h-full")}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div className="flex h-full flex-col">
+            <Header />
+            <div className="flex-1 max-w-lg m-auto py-12 w-full">
+              {children}
+            </div>
+            <Footer />
+          </div>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
